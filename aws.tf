7bd82c18c0d44e682da8f454952546a2ad6e7853
sivakumarkandregula <91835322+sivakumarkandregula@users.noreply.github.com>
2023-09-06 10:22:14 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,3 +1,10 @@
-ami= "ami-0f409bae3775dc8e5"
-instance_type = "t2.micro"
-count = 1
+resource "aws_instance" "siva_instance" {
+    #Argument Block
+    ami= "ami-0f409bae3775dc8e5"
+    instance_type = "t2.micro"
+    count = 1
+
+    tags = {
+        Name = "siva_vm"
+    }
+}
